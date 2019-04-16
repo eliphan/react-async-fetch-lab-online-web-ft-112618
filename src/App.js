e6986b22f17a9c2b7657a9b75283491ce86e223e
Elise Phan <aliceconnic@gmail.com>
2019-04-16 02:35:35 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React from 'react';
 
 class App extends Component {
   
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
 }
 
 export default App
